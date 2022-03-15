642c1f366e712ec496e640085fd1fd302c7f58e8
J <joras.custodio@gmail.com>
2022-03-15 09:22:16 -0300

    added some notes, templates and: base.html, index.html

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,10 @@
+from turtle import title
 from django.db import models
 
 class Note(models.Model):
+    
     title = models.CharField(max_length=200)
     content = models.TextField()
+
+    def __str__(self):
+        return(f"{self.id}. {self.title}")
